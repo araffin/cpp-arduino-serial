3bbe32682514bd6dfde33af1c94b84dd07b08b52
Antonin <antonin.raffin@ensta-paristech.fr>
2018-03-30 19:11:17 +0200

    Doc

--- lib/robust_serial.cpp
+++ lib/robust_serial.cpp
@@ -3,50 +3,28 @@
 using namespace std;
 
 
-/**
- * Send one order (one byte) to the other arduino
- * @param myOrder type of order
- */
-void write_order(fstream &file, enum Order myOrder)
+void write_order(fstream &file, enum Order order)
 {
-	write_i8(file, (int8_t) myOrder);
+	write_i8(file, (int8_t) order);
 }
 
-/**
- * Send a int of one byte
- * @param num
- */
 void write_i8(fstream &file, int8_t num)
 {
-	int8_t* oneByte = (int8_t*) &num;
-  file.write((char *)oneByte, sizeof(int8_t));
+  file.write((char *)&num, sizeof(int8_t));
 }
 
-
-/**
- * Send a two bytes signed int via the serial
- * @param num the number to send
- */
 void write_i16(fstream &file, int16_t num)
 {
 	int8_t buffer[2] = {(int8_t) (num & 0xff), (int8_t) (num >> 8)};
 	file.write((char *)buffer, 2*sizeof(int8_t));
 }
 
-/**
- * Send a four bytes signed int (long) via the serial
- * @param num the number to send (−2,147,483,647, +2,147,483,647)
- */
 void write_i32(fstream &file, int32_t num)
 {
 	int8_t buffer[4] = {(int8_t) (num & 0xff), (int8_t) (num >> 8 & 0xff), (int8_t) (num >> 16 & 0xff), (int8_t) (num >> 24 & 0xff)};
   file.write((char *)buffer, 4*sizeof(int8_t));
 }
 
-/**
- * Send a two bytes unsigned (max 2**16 -1) int via the serial
- * @param num the number to send
- */
 void write_u32(fstream &file, uint16_t num)
 {
 	uint8_t buffer[2] = {(uint8_t) (num & 0xff), (uint8_t) (num >> 8)};
@@ -54,57 +32,38 @@ void write_u32(fstream &file, uint16_t num)
 }
 
 
-/**
- * Read two bytes (16 bits) from the serial and convert it to an unsigned int
- * @return the decoded unsigned int
- */
-uint16_t read_u16(SerialPort &serial_port)
-{
-  SerialPort::DataBuffer charBuff;
-  serial_port.Read(charBuff, 2);
-  return (uint16_t)(charBuff[0] & 0xff) | (uint16_t)(charBuff[1] << 8);
-}
-/**
- * Read two bytes from the serial and convert it to an int
- * @return the decoded int
- */
-int16_t read_i16(SerialPort &serial_port)
-{
-  SerialPort::DataBuffer charBuff;
-  serial_port.Read(charBuff, 2);
-  return (int16_t)(charBuff[0] & 0xff) | (int16_t)(charBuff[1] << 8 & 0xff00);
-}
-
-/**
- * Read four bytes from the serial and convert it to an long
- * @return the decoded int
- */
-int32_t read_i32(SerialPort &serial_port)
-{
-  SerialPort::DataBuffer charBuff;
-  serial_port.Read(charBuff, 4);
-  return (int8_t)(charBuff[0] & 0xff) | (int8_t)(charBuff[1] << 8 & 0xff00) | (int8_t)(charBuff[2] << 16 & 0xff0000) | (int8_t)(charBuff[3] << 24 & 0xff000000);
-}
-
-/**
- * Read one byte from the serial and convert it to an int
- * @return the decoded int
- */
-int8_t read_i8(SerialPort &serial_port)
-{
-  SerialPort::DataBuffer charBuff;
-  serial_port.Read(charBuff, 1);
-  // We have to cast it to keep the sign
-  return (int8_t) static_cast<signed char>(charBuff[0]);
-}
-
-/**
- * Read one byte from the serial and cast it to an Order
- * @return the order received
- */
-Order read_order(SerialPort &serial_port, const unsigned int msTimeout)
+Order read_order(SerialPort &serial_port, const unsigned int timeout_ms)
 {
   SerialPort::DataBuffer buffer;
-  serial_port.Read(buffer, 1, msTimeout);
+  serial_port.Read(buffer, 1, timeout_ms);
   return (Order) buffer[0];
+}
+
+int8_t read_i8(SerialPort &serial_port)
+{
+  SerialPort::DataBuffer buffer;
+  serial_port.Read(buffer, 1);
+  // We have to cast it to keep the sign
+  return (int8_t) static_cast<signed char>(buffer[0]);
+}
+
+int16_t read_i16(SerialPort &serial_port)
+{
+  SerialPort::DataBuffer buffer;
+  serial_port.Read(buffer, 2);
+  return (int16_t)(buffer[0] & 0xff) | (int16_t)(buffer[1] << 8 & 0xff00);
+}
+
+uint16_t read_u16(SerialPort &serial_port)
+{
+  SerialPort::DataBuffer buffer;
+  serial_port.Read(buffer, 2);
+  return (uint16_t)(buffer[0] & 0xff) | (uint16_t)(buffer[1] << 8);
+}
+
+int32_t read_i32(SerialPort &serial_port)
+{
+  SerialPort::DataBuffer buffer;
+  serial_port.Read(buffer, 4);
+  return (int8_t)(buffer[0] & 0xff) | (int8_t)(buffer[1] << 8 & 0xff00) | (int8_t)(buffer[2] << 16 & 0xff0000) | (int8_t)(buffer[3] << 24 & 0xff000000);
 }
--- lib/robust_serial.hpp
+++ lib/robust_serial.hpp
@@ -5,16 +5,72 @@
 #include <SerialStream.h>  // libserial-dev
 #include "order.h"
 
-void write_order(std::fstream &file, enum Order myOrder);
+/**
+ * Send one order (one byte) to the other arduino
+ * @param order the order to send
+ */
+void write_order(std::fstream &file, enum Order order);
+
+/**
+ * Write one byte int to a file/serial port
+ * @param file fstream object
+ * @param num
+ */
 void write_i8(std::fstream &file, int8_t num);
+
+/**
+ * Write two bytes int to a file/serial port.
+ * @param file fstream object
+ * @param num the number to send
+ */
 void write_i16(std::fstream &file, int16_t num);
-void write_u16(std::fstream &file, uint16_t num);
+
+/**
+ * Write four bytes int to a file/serial port.
+ * @param num the number to send (−2,147,483,647, +2,147,483,647)
+ */
 void write_i32(std::fstream &file, int32_t num);
 
-Order read_order(SerialPort &serial_port, const unsigned int msTimeout);
+/**
+ * Send a two bytes unsigned (max 2**16 -1) int via the serial
+ * @param num the number to send
+ */
+void write_u32(std::fstream &file, uint16_t num);
+
+/**
+ * Read one byte from the serial and cast it to an Order
+ * @param  serial_port SerialPort object
+ * @param  msTimeout   Read Timeout in ms
+ * @return the order received
+ */
+Order read_order(SerialPort &serial_port, const unsigned int timeout_ms);
+
+/**
+ * Read one byte from a serial port and convert it to a 8 bits int
+ * @param  serial_port SerialPort object
+ * @return the decoded number
+ */
 int8_t read_i8(SerialPort &serial_port);
+
+/**
+ *  Read two bytes from a serial port and convert it to a 16 bits int
+ * @param  serial_port SerialPort object
+ * @return the decoded number
+ */
 int16_t read_i16(SerialPort &serial_port);
+
+/**
+ * Read two bytes from a serial port and convert it to a 16 bits unsigned int
+ * @param  serial_port SerialPort object
+ * @return the decoded number
+ */
 uint16_t read_u16(SerialPort &serial_port);
+
+/**
+ * Read four bytes from a serial port and convert it to a 32 bits int
+ * @param  serial_port SerialPort object
+ * @return the decoded number
+ */
 int32_t read_i32(SerialPort &serial_port);
 
 #endif
